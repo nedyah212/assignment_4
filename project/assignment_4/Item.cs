99d73eb365ba4c68b744d58c0dcf65d3acf3f6a1
Hayden Ploszay <123116599+nedyah212@users.noreply.github.com>
2025-11-30 14:00:17 -0600

    completed item class

---
+++ project/assignment_4/Item.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_4
+{
+	public class Item : IComparable<Item>
+	{
+		public string Name;
+		public int GoldPieces;
+		public double Weight;
+
+		public Item(string name, int goldPieces, double weight)
+		{
+			Name = name;
+			GoldPieces = goldPieces;
+			Weight = weight;
+		}
+
+		public override bool Equals(object obj)
+		{
+			bool result = true;
+			try
+			{
+				if (obj == null)
+					throw new ArgumentNullException("The object cannot be null");
+
+				if (obj.GetType() != this.GetType())
+				{
+					throw new ArgumentException("The object has to be an item");	
+				}
+
+				Item objAsItem = (Item)obj;
+				
+				if (objAsItem.Name != this.Name)
+					result = false;
+				if (objAsItem.GoldPieces != this.GoldPieces)
+					result = false;
+				if (objAsItem.Weight != this.Weight)
+					result = false;
+			}
+			catch (ArgumentNullException)
+			{
+				result = false;
+			}
+			
+			catch (ArgumentException)
+			{
+				result = false;
+			}
+
+			return result;
+		}
+
+		public int CompareTo(Item other)
+		{
+			if (other == null)
+				return 1;
+			else
+			{
+				return this.Name.CompareTo(other.Name);
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{this.Name} is worth {this.GoldPieces}gp and weighs {this.Weight}kg";
+		}
+	}
+}
