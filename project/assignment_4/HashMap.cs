8175cd4e29da54e4e5739a0aa4d05e27adf6a027
Hayden Ploszay <123116599+nedyah212@users.noreply.github.com>
2025-11-30 22:04:32 -0600

    started working on hashmap class, completed up to Placeholders

--- project/assignment_4/HashMap.cs
+++ project/assignment_4/HashMap.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace assignment_4
 {
-	public class HashMap
+	public class HashMap<K, V>
 	{
+		public Entry<K, V> [] Table { get; set;}
+
+		int Size { get; set;}
+		int Placeholders { get; set;}
+
+		const int CAPACITY = 11;
+		const double LOAD_FACTOR = 0.75;
+
+		int InitialCapacity;
+		double LoadFactor;
+
+		public HashMap()
+		{
+			InitialCapacity = CAPACITY;
+			LoadFactor = LOAD_FACTOR;
+		}
+
+		public HashMap(int initialCapacity)
+		{ 
+			InitialCapacity = initialCapacity;
+			LoadFactor = LOAD_FACTOR;
+		}
+
+		public HashMap(double loadFactor)
+		{
+			InitialCapacity = CAPACITY;
+			LoadFactor = loadFactor;
+		}
 	}
 }
